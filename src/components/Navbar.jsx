6c37a453df8925a112dc5a1fee6e79192f29399b
Shubham Panwar <shubham526527@gmail.com>
2025-01-30 17:07:23 +0530

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,6 @@ const Navbar = () => {
   return (
     <div className="font-sans">
       <nav className="flex flex-row items-center justify-between bg-[#021020] text-white py-4 px-6 md:px-12">
-        {/* Logo and Brand Name */}
         <a href="/" className="flex items-center space-x-3">
           <img className="w-16 h-16" src={logo} alt="FitSync Logo" />
           <h1 className="text-2xl md:text-3xl font-semibold text-white">
@@ -14,9 +13,7 @@ const Navbar = () => {
           </h1>
         </a>
 
-        {/* Navigation Links and Dropdown */}
         <div className="flex items-center space-x-6 mr-4 md:mr-0">
-          {/* Navigation Links (Hidden on Small Screens) */}
           <ul className="hidden md:flex space-x-6">
             <li className="hover:text-gray-400 transition duration-300">
               <a href="/" className="text-lg">
@@ -35,7 +32,6 @@ const Navbar = () => {
             </li>
           </ul>
 
-          {/* Dropdown (Always Visible) */}
           <div className="relative">
             <Dropdown />
           </div>
